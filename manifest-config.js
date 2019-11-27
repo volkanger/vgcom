4ca4deb69296a82b697eaa5ee31731b2f822dfe1
Volkan Ger <volkanger@gmail.com>
2019-11-27 13:50:29 -0500

    tool name and title

--- manifest-config.js
+++ manifest-config.js
@@ -1,8 +1,8 @@
 const colors = require('./colors');
 
 module.exports = {
-  name: 'Mate Gatsby Starter',
-  short_name: 'Mate Gatsby Starter',
+  name: 'Volkan Ger\'s Website',
+  short_name: 'VG.com',
   start_url: '/',
   background_color: colors.background,
   theme_color: colors.primary,
