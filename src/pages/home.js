032d93184833744396b6094cee2fd210c7ec4ae8
Volkan Ger <volkanger@gmail.com>
2019-11-28 00:18:52 -0500

    testing additional pages

---
+++ src/pages/home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import Landing from '../sections/Landing';
+import About from '../sections/About';
+import Projects from '../sections/Projects';
+import Writing from '../sections/Writing';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const HomePage = () => (
+  <Layout>
+    <Header />
+    <Landing />
+    <About />
+    <Landing />
+    <Landing />
+    <Landing />
+    <Projects />
+    <Writing />
+    <Landing />
+    <Footer />
+  </Layout>
+);
+
+export default HomePage;
